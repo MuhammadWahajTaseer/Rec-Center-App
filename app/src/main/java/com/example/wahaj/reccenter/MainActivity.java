39b250beb65aed8c6b109a9e12514d04b22d81e7
wahaj taseer <wahaj taseer>
2016-11-10 19:13:19 -0700

    first commit

---
+++ app/src/main/java/com/example/wahaj/reccenter/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.wahaj.reccenter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    // Ripple effect only shows when there is a method associated with it
+    public void FitnessPressed(View view) {
+
+    }
+
+    public void aquaticPressed(View view) {
+    }
+
+    public void climbingPressed(View view) {
+    }
+
+    public void courtsPressed(View view) {
+    }
+
+    public void olympicovalPressed(View view) {
+    }
+
+    public void outdoorPressed(View view) {
+    }
+
+    public void levelupPressed(View view) {
+    }
+
+    public void programsPressed(View view) {
+    }
+
+}
